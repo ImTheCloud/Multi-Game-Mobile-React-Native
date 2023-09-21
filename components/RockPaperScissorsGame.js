ac2afe5287bd7d62a622ff31cb0c14b805bfbde4
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-09-21 12:17:11 +0200

    rock paper

--- components/RockPaperScissorsGame.js
+++ components/RockPaperScissorsGame.js
@@ -1,58 +1,68 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 export default function RockPaperScissorsGame() {
   const [userChoice, setUserChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
   const [result, setResult] = useState('');
 
-  const choices = ['Rock', 'Paper', 'Scissors'];
+  const choices = [
+    { image: require('../assets/rock.png'), text: 'Rock' },
+    { image: require('../assets/paper.png'), text: 'Paper' },
+    { image: require('../assets/scissors.png'), text: 'Scissors' },
+    { image: require('../assets/puit.png'), text: 'Puit' },
+  ];
 
   const generateComputerChoice = () => {
-    const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
+    const randomIndex = Math.floor(Math.random() * 4);
+    return randomIndex;
   };
 
-  const determineWinner = (user, computer) => {
-    if (user === computer) return "It's a tie!";
+  const determineWinner = (userIndex, computerIndex) => {
+    if (userIndex === computerIndex) return "It's a tie!";
+    if (userIndex === 3) return 'You win!';
     if (
-      (user === 'Rock' && computer === 'Scissors') ||
-      (user === 'Paper' && computer === 'Rock') ||
-      (user === 'Scissors' && computer === 'Paper')
+      (userIndex === 0 && computerIndex === 2) ||
+      (userIndex === 1 && computerIndex === 0) ||
+      (userIndex === 2 && computerIndex === 1)
     ) {
       return 'You win!';
     } else {
-      return 'Computer wins!';
+      return 'IA wins!';
     }
   };
 
-  const handleChoice = (choice) => {
-    const computerChoice = generateComputerChoice();
-    const result = determineWinner(choice, computerChoice);
+  const handleChoice = (index) => {
+    const computerIndex = generateComputerChoice();
+    const result = determineWinner(index, computerIndex);
 
-    setUserChoice(choice);
-    setComputerChoice(computerChoice);
+    setUserChoice(index);
+    setComputerChoice(computerIndex);
     setResult(result);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Rock Paper Scissors</Text>
       <View style={styles.choices}>
-        {choices.map((choice) => (
+        {choices.map((choice, index) => (
           <TouchableOpacity
-            key={choice}
+            key={index}
             style={styles.choiceButton}
-            onPress={() => handleChoice(choice)}
+            onPress={() => handleChoice(index)}
           >
-            <Text style={styles.choiceText}>{choice}</Text>
+            <Image source={choice.image} style={styles.choiceImage} />
+            <Text style={styles.choiceText}>{choice.text}</Text>
           </TouchableOpacity>
         ))}
       </View>
-      {userChoice && computerChoice && (
+      {userChoice !== null && computerChoice !== null && (
         <View style={styles.result}>
-          <Text style={styles.resultText}>You chose: {userChoice}</Text>
-          <Text style={styles.resultText}>Computer chose: {computerChoice}</Text>
+          <Text style={styles.resultText}>
+            You chose: {choices[userChoice].text}
+          </Text>
+          <Text style={styles.resultText}>
+            Computer chose: {choices[computerChoice].text}
+          </Text>
           <Text style={styles.resultText}>{result}</Text>
         </View>
       )}
@@ -77,12 +87,13 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   choiceButton: {
-    backgroundColor: '#007AFF',
-    padding: 10,
-    borderRadius: 5,
+    alignItems: 'center',
+  },
+  choiceImage: {
+    width: 95, // Ajustez cette taille en fonction de vos besoins
+    height: 100, // Ajustez cette taille en fonction de vos besoins
   },
   choiceText: {
-    color: 'white',
     fontSize: 18,
   },
   result: {
