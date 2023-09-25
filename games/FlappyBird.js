4b91e8d8fc40146a40582eb9f8914e6edd47faca
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-09-25 16:39:05 +0200

    designe flappy

--- games/FlappyBird.js
+++ games/FlappyBird.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, ImageBackground } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
 import entities from '../components/FlappybirdComponents/entities';
 import Physics from '../components/FlappybirdComponents/physics';
 import { TouchableOpacity } from 'react-native';
 
 const birdImage = require('../assets/flappybird.png');
+const backgroundImage = require('../assets/background.png');
 
 function FlappyBird() {
   const [running, setRunning] = useState(false);
@@ -19,48 +20,53 @@ function FlappyBird() {
 
   return (
     <View style={{ flex: 1 }}>
-      <Text style={{ textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20 }}>
-        {currentPoints}
-      </Text>
-      <GameEngine
-        ref={(ref) => {
-          setGameEngine(ref);
-        }}
-        systems={[Physics]}
-        entities={entities(birdImage)} // Pass the bird image as a prop
-        running={running}
-        onEvent={(e) => {
-          switch (e.type) {
-            case 'game_over':
-              setRunning(false);
-              gameEngine.stop();
-              break;
-            case 'new_point':
-              setCurrentPoints(currentPoints + 1);
-              break;
-          }
-        }}
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+      <ImageBackground // Utilisez ImageBackground pour définir l'image de fond
+        source={backgroundImage}
+        style={{ flex: 1 }}
       >
-        <StatusBar style="auto" hidden={true} />
-      </GameEngine>
+        <Text style={{ textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20 }}>
+          {currentPoints}
+        </Text>
+        <GameEngine
+          ref={(ref) => {
+            setGameEngine(ref);
+          }}
+          systems={[Physics]}
+          entities={entities(birdImage)} // Pass the bird image as a prop
+          running={running}
+          onEvent={(e) => {
+            switch (e.type) {
+              case 'game_over':
+                setRunning(false);
+                gameEngine.stop();
+                break;
+              case 'new_point':
+                setCurrentPoints(currentPoints + 1);
+                break;
+            }
+          }}
+          style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+        >
+          <StatusBar style="auto" hidden={true} />
+        </GameEngine>
 
-      {!running ? (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <TouchableOpacity
-            style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
-            onPress={() => {
-              setCurrentPoints(0);
-              setRunning(true);
-              gameEngine.swap(entities(birdImage)); // Pass the bird image as a prop
-            }}
-          >
-            <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
-              START GAME
-            </Text>
-          </TouchableOpacity>
-        </View>
-      ) : null}
+        {!running ? (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity
+              style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
+              onPress={() => {
+                setCurrentPoints(0);
+                setRunning(true);
+                gameEngine.swap(entities(birdImage)); // Pass the bird image as a prop
+              }}
+            >
+              <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
+                START GAME
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ) : null}
+      </ImageBackground>
     </View>
   );
 }
