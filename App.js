9338e6e13d717ad2ad8481362e62fc6a82b9a180
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-10-03 13:58:50 +0200

    login/register

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './components/HomeScreen';
 import TicTacToe from './games/TicTacToe';
 import RockPaperScissorsGame from './games/RockPaperScissorsGame';
 import HangmanGame from './games/HangmanGame';
 import FlappyBird from './games/FlappyBird';
+import LoginScreen from './screens/LoginScreen';
+import HHomeScreen from './screens/HomeScreen';
 
 const Stack = createStackNavigator(); //gere la navigation
 
@@ -13,12 +16,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
         <Stack.Screen name="TicTacToe" component={TicTacToe} />
         <Stack.Screen name="RockPaperScissorsGame" component={RockPaperScissorsGame} />
         <Stack.Screen name="HangmanGame" component={HangmanGame} />
         <Stack.Screen name="FlappyBird" component={FlappyBird} />
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HHomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASJvnkHpf8FEV8k_MeIr9hM2ZbHVxCeAg",
+  authDomain: "multi-game-mobile.firebaseapp.com",
+  projectId: "multi-game-mobile",
+  storageBucket: "multi-game-mobile.appspot.com",
+  messagingSenderId: "615468934709",
+  appId: "1:615468934709:web:9b3c3dc50d51621867737c"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
